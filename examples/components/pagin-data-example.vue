9d3274880925bc80255acce8a20a069d7c55de2a
shiyuan.tian <tsy821@qq.com>
2022-02-24 19:00:09 +0800

    更新example的描述

--- examples/components/pagin-data-example.vue
+++ examples/components/pagin-data-example.vue
@@ -18,10 +18,29 @@
       template(slot-scope='scope') 
         .data-row(v-for='(row, idx) of scope.data' :key='idx') {{row}}
 
-    p  
+    p 
+      p 分页查询在系统中占一半的开发工作量，如果每个页面都独立开发，成本很高，而且一致性比较差，每个人的实现看似一样，但又多少有些不同，这些差异会让整个系统的口碑下降，给人留下项目管理混乱的印象。
+      p 撇开layout的部分不谈， 检索条件变化自动查询，翻页，调用查询接口这些行为应该都是高度一致的，sy-pagin-data就是把这部分高度一致的控制抽成了一个组件，组件用户不用处理这部分逻辑，只要把检索用的接口地址通过props传入，就天然的拥有了检索、分页等行为。通过slot传入用于数据渲染的部分，分页组件会把对应页面的数据，通过slot-scope传给数据展示组件，大家专心做好每一条记录绘制就OK了。
+      img(:src='paginDataExampleCodeJpg')
+    
+    p 
+      p url: 返回数据的接口地址。
+      p 接口应为get请求，如果不是，可以通过传入requestMethod:String 指定请求方法
+      p 接口应返回这样的数据格式。
+      .block
+        .p {
+          .p data: Array, // 检索结果的集合
+          .p total: Number  // 匹配检索条件的记录总数
+        .p }
+      p 如果服务器返回数据接口不满足上面的要求，可以通过传入respDataMapper:Function 对返回的数据做处理,并返回处理结果
+      p 组件会在url的请求参数中自动添加pageNo:Number(当前页码) 和 pageSize:Number(请求数据数)，如果后端接口使用的不是这两个名称，可以通过传入paginDataMapper:Function 来替换成相应的名称。
+
+  
 </template>
 
 <script>
+import paginDataExampleCodeJpg from "../assets/pagin-data-example-code.jpg";
+
 export default {
   data() {
     return {
@@ -32,7 +51,9 @@ export default {
     };
   },
   created() {
-    (this.mockDataFunc = (cfg) => {
+    this.paginDataExampleCodeJpg = paginDataExampleCodeJpg;
+
+    this.mockDataFunc = (cfg) => {
       console.log(cfg);
       const { params } = cfg;
       const { pageNo, pageSize } = params;
@@ -48,11 +69,11 @@ export default {
         data,
         total: 993,
       };
-    }),
-      (this.paginDataMapper = (data) => {
-        console.log(data);
-        return data;
-      });
+    };
+    this.paginDataMapper = (data) => {
+      console.log(data);
+      return data;
+    };
   },
 };
 </script>
@@ -67,4 +88,7 @@ export default {
   user-select: none
 .data-row:hover
   background-color: #e0e0e0
+
+.p
+  padding: 10px
 </style>
--- examples/components/pagin-table-example.vue
+++ examples/components/pagin-table-example.vue
@@ -1,5 +1,6 @@
 <template lang="pug">
   .pagin-table-example-main
+    .h1 分页Table  sy-pagin-table
     .criteria-bar
       el-input.w200(v-model='query.keyword')
       | &nbsp;
@@ -15,6 +16,11 @@
       style='width:1000px'
     )
       template(v-slot:age='scope') {{scope.data.row.age}}
+    
+    p
+      p 因为大部分的查询页面是以表格的形式绘制的数据，所以我们把sy-pagin-data 和 sy-table做了一下融合，形成了sy-pagin-table，支持的props 就是这两个组件的合集。
+      sy-table(:columns='propTableColumns' :data='propTableData')
+
 </template>
 
 <script>
@@ -22,55 +28,101 @@ export default {
   data() {
     return {
       query: {
-        keyword: '关键字'
-      }
-    }
+        keyword: "关键字",
+      },
+    };
   },
   created() {
+    this.propTableColumns = [
+      {
+        label: "名称",
+        prop: "name",
+        width: 100,
+      },
+      {
+        label: "类型",
+        prop: "type",
+        width: 100,
+      },
+      {
+        label: "说明",
+        prop: "comment",
+      },
+    ];
+    this.propTableData = [
+      {
+        name: "url",
+        type: "String",
+        comment: "检索接口的地址",
+      },
+      {
+        name: "request-method",
+        type: "String",
+        comment: "调用接口的请求方法,默认为GET",
+      },
+      {
+        name: "query",
+        type: "Plain Object",
+        comment: "检索挑拣对象，发起ajax请求时作为参数传给后端",
+      },
+      {
+        name: "columns",
+        type: "Array[Object]",
+        comment:
+          "列定义对象{label: String, prop: String, getContent: Function, slot: String}, label是列的展示名称，prop是用来从行数据中取值的key，getContent是取值的方法，接手当前行作为参数，如果一个列由多个字段组成，或者字段的数据需要处理，可以利用该函数实现. slot的作用是创建一个命名插槽，如果这个列渲染比较复杂，不如是有交互行为的按钮，链接等，可以通过命名插槽实现。当前行会作为data绑定在插槽上。",
+      },
+      {
+        name: "pagin-data-mapper",
+        type: "Function",
+        comment:
+          "如果后端接受的分页信息不是pageNo和pageSize，可以利用这个钩子函数，在发起ajax请求前，进行更名。",
+      },
+      {
+        name: "resp-data-mapper",
+        type: "Function",
+        comment:
+          "paginTable期待服务器返回这样的应该格式{data:Array, total:Number},如果接口返回的不是这样的格式，可以利用这个钩子函数对返回的数据做变形，并返回 这个格式。",
+      },
+    ];
     this.columns = [
       {
-        label: '姓名',
-        prop: 'name'
+        label: "姓名",
+        prop: "name",
       },
       {
-        label: '性别',
+        label: "性别",
         getContent(data) {
-          return data.row.gender
-        }
+          return data.row.gender;
+        },
       },
       {
-        label: '年龄',
-        slot: 'age'
-      }
-    ]
-    this.mockDataFunc = cfg => {
-      console.log(cfg)
-      const {
-        params
-      } = cfg
-      const {
-        pageNo,
-        pageSize
-      } = params
-      const data =  []
-      for (let idx=0; idx<pageSize; idx++) {
+        label: "年龄",
+        slot: "age",
+      },
+    ];
+    this.mockDataFunc = (cfg) => {
+      console.log(cfg);
+      const { params } = cfg;
+      const { pageNo, pageSize } = params;
+      const data = [];
+      for (let idx = 0; idx < pageSize; idx++) {
         data.push({
           name: `${pageNo} name`,
           gender: `${pageNo} gender`,
           age: `${pageNo} age`,
-        })
+        });
       }
       return {
         data,
-        total: 993
-      }
-    },
-    this.paginDataMapper = data => {
-      console.log(data) // 结构不变变可以不提供这个mapper
-      return data
-    }
-  }
-}
+        total: 993,
+      };
+    };
+    this.paginDataMapper = (data) => {
+      console.log(data); // 结构不变变可以不提供这个mapper
+      return data;
+    };
+  },
+};
 </script>
 
 <style lang="sass" scoped>
