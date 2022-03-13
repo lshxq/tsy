d4ae59745abb626ba686030952a5c1609ec03c00
shiyuan.tian <tsy821@qq.com>
2022-03-13 10:29:35 +0800

    v.0.1.4 panel resize

--- examples/views/example-panel.vue
+++ examples/views/example-panel.vue
@@ -5,6 +5,9 @@
       .item(v-for='row of rows' :key='row') {{row}}
 
     .mt100
+      p 面板增加的一下内容
+      p mount事件，标示面板已经载入dom
+      p resize事件，当面板大小改变时，事件接受两个参数，分别是当前size和变化前的size。
 </template>
 
 <script>
