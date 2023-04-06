d3f70bc790b838cc97ae7df03058dc01b2a5be26
tsy <tsy821@qq.com>
2023-04-06 10:50:28 +0800

    update calendar

--- examples/views/example-calendar.vue
+++ examples/views/example-calendar.vue
@@ -2,6 +2,9 @@
   <div class="example-calendar-main">
     <div class="calendar-container"><sy-calendar v-model="date"/></div>
     <div class="current-value">当前值： {{dateStr}}</div>
+    <div class="mt30">
+      一个简朴的日历组件，通过v-model绑定当前选择的日期，默认值为今天。日历的宽度通过父容器确定。 &lt;sy-calendar v-model="date"/&gt;
+    </div>
   </div>
 </template>
 
